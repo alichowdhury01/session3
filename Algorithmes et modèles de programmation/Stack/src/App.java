78e6f3f109f132ad3f90bd918ebe7a4b7aa0f2d8
alichowdhury01 <alichowdhury01@gmail.com>
2022-09-06 13:03:10 -0400

    Pdf de tavares

---
+++ Algorithmes et modèles de programmation/Stack/src/App.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Stack<String> stack = new Stack<String>();
+
+
+        stack.push("1");
+        stack.push("2");
+        stack.push("3");
+        stack.push("4");
+        stack.push("5");
+
+        System.out.println(stack.isEmpty());
+
+    }
+}
