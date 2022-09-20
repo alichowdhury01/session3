900993270646cbba9a462afc6fad6f0f61bb927e
alichowdhury01 <39740365+alichowdhury01@users.noreply.github.com>
2022-09-20 17:42:30 -0400

    d

--- Algorithmes et modèles de programmation/Labo1/src/BiblioTab.java
+++ Algorithmes et modèles de programmation/Labo1/src/BiblioTab.java
@@ -1,3 +1,4 @@
-public class BiblioTab {
+public class BiblioTab extends Bibliotheque {
+
     
 }
--- Algorithmes et modèles de programmation/Labo1/src/Bibliotheque.java
+++ Algorithmes et modèles de programmation/Labo1/src/Bibliotheque.java
@@ -1,3 +1,5 @@
+// La classe Bibliotheque sera simplement une version abstraite déclarant les mêmes méthodes que BiblioTab mais sans les implémenter.
 public class Bibliotheque {
+    
     
 }
